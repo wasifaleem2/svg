2a4795d89cc5a772e94c7eed08fe097b5bd5b257
wasif <wasif.aleem@braincrop.net>
2023-06-14 10:59:29 +0500

    galaxy

---
+++ index.js
@@ -0,0 +1,27 @@
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const minHeight = Math.min(height[left], height[right]);
+        const area = width * minHeight;
+
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+}
+let height = [1,8,6,2,5,4,8,3,7]
+let ans = maxArea(height);
+console.log(ans)
+
+
+
